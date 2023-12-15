796ed169276a55a3136049f10664618b346bb5af
kevin <kevinmbari600@gmail.com>
2023-12-15 18:44:46 +0300

    routes for daily track updated

--- main.py
+++ main.py
@@ -20,13 +20,27 @@ def get_patient():
 @app.post('/patientform')
 def create_patient():
     return {"message": "patient created succesfully"}
+#posting dailytrack
+@app.post('/dailytracks')
+def create_patient():
+    return {"message": "daily track submitted succesfully"}
 
 #updating a patient
 @app.patch('/patientform/{patient_id}')
 def update_patient(patient_id: int):
     return {"message":f"patient {patient_id} created succesfully"}
 
+#updating the daily track
+@app.patch('/dailytracks/{patient_id}')
+def update_patient(patient_id: int):
+    return {"message":f"daily track of patient {patient_id} created succesfully"}
+
 #deleting a patient
 @app.delete('/patientform/{patient_id}')
 def update_patient(patient_id: int):
     return {"message":f"patient {patient_id} deleted succesfully"}
+
+#deleting a daily track
+@app.delete('/dailytracks/{patient_id}')
+def update_patient(patient_id: int):
+    return {"message":f"daily track of patient  {patient_id} deleted succesfully"}
